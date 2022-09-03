0fef727bcd3836ffa92398fd6d3dc602c26e6bbe
whleng <lengwenhui@gmail.com>
2022-09-03 14:38:55 -0400

    edited grid

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,28 +1,22 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { makeStyles } from '@material-ui/core/styles'
-import {
-    Grid,
-    CardContent,
-    Typography,
-    CardHeader
-} from '@material-ui/core/'
-import RestaurantCard from './components/RestaurantCard';
+import RestaurantCard from '../components/RestaurantCard';
+// import Grid from '@material-ui/core/';
 
 const HomeScreen = () => {
   return (
-    <Grid container spacing={24}>
-      <Grid item md={3}>
-        RestaurantCard()
-        RestaurantCard()
-        RestaurantCard()
-      </Grid>
-      <Grid item md={3}>
-      </Grid>
-      <Grid item md={3}>
-      </Grid>
-    </Grid>
+    // <Grid container spacing={24}>
+    //   <Grid item md={3}>
+    RestaurantCard(),
+    RestaurantCard(),
+    RestaurantCard()
+    //   </Grid>
+    //   <Grid item md={3}>
+    //   </Grid>
+    //   <Grid item md={3}>
+    //   </Grid>
+    // </Grid>
   )
 }; 
 export default HomeScreen;
