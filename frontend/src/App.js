4b77fd965ad6b5d1e918dfed47791ba8e5da3646
kaitlyncliu <liu.kaitlync@gmail.com>
2022-09-03 10:49:15 -0400

    import libraries and start navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			<BrowserRouter>
+				<Navbar variant="tabs" defaultActiveKey="/home">
+					<Nav.Item>
+						<Nav.Link href="/home">Active</Nav.Link>
+					</Nav.Item>
+					<Nav.Item>
+						<Nav.Link eventKey="link-1">Option 2</Nav.Link>
+					</Nav.Item>
+					<Nav.Item>
+						<Nav.Link eventKey="disabled" disabled>
+							Disabled
+						</Nav.Link>
+					</Nav.Item>
+				</Navbar>
+			</BrowserRouter>
 		</div>
 	);
 }
