b11d2f9de281016b4b9bde9c141b2923228b875b
syn0206 <91432286+syn0206@users.noreply.github.com>
2022-09-03 19:27:24 -0400

    Added restaurant screen (incomplete)

--- frontend/src/screens/RestaurantScreen.js
+++ frontend/src/screens/RestaurantScreen.js
@@ -1,5 +1,34 @@
-import { useEffect, useReducer } from 'react';
+import Accordion from 'react-bootstrap/Accordion';
+import Image from 'react-bootstrap/Image';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Stack from 'react-bootstrap/Stack';
 
-export default function RestaurantScreen() {
-	return <></>;
+
+function RestaurantInfo() {
+	return (
+		<Container>
+			<Row>
+				<Col><Image src="___" rounded /></Col>
+			</Row>
+			<Row>
+				<Stack gap={2}>
+      				<div className="bg-light border">Name__</div>
+      				<div className="bg-light border">Address__</div>
+      				<div className="bg-light border">Relative Portion Size__</div>
+    			</Stack>
+			</Row>
+			<Row>
+				<Accordion>
+					<Accordion.Item eventKey="0">
+						<Accordion.Header>Description</Accordion.Header>
+						<Accordion.Body>___</Accordion.Body>
+					</Accordion.Item>
+				</Accordion>
+			</Row>
+		</Container>
+	)
 }
+
+export default RestaurantInfo;
