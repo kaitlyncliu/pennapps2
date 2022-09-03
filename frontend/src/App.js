e85253d3fc39851d838c902812c08596f9a84c54
whleng <lengwenhui@gmail.com>
2022-09-03 15:28:49 -0400

    updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,30 +2,41 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
-import {useState} from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
 import HomeScreen from './screens/HomeScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import RestaurantScreen from './screens/RestaurantScreen';
+import ShareScreen from './screens/ShareScreen';
 import Button from 'react-bootstrap/Button';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<header>
-				<Nav expand="lg">
-					<Nav.Item>
-						<Link to="/">Home</Link>
-					</Nav.Item>
-					<Nav.Item>
-						<Link to="/profile">Profile</Link>
-					</Nav.Item>
-					<Nav.Item>
-						<Link to="/social">Social</Link>
-					</Nav.Item>
-				</Nav>
+				<Navbar fixed="bottom" bg="dark" variant="dark" expand="lg">
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Nav>
+						<Container>
+							<Nav.Link href="/">Home</Nav.Link>
+						</Container>
+						<Container>
+							<Nav.Link href="/profile">Profile</Nav.Link>
+						</Container>
+						<Container>
+							<Nav.Link href="/social">Social</Nav.Link>
+						</Container>
+					</Nav>
+				</Navbar>
 			</header>
 			<main>
-				<Container></Container>
+				<Container>
+					<Routes>
+						<Route path="/" element={HomeScreen} />
+						<Route path="/profile" element={ProfileScreen} />
+						<Route path="/restaurants/:id" element={RestaurantScreen} />
+						<Route path="/share" element={ShareScreen.js} />
+					</Routes>
+				</Container>
 			</main>
 		</BrowserRouter>
 	);
